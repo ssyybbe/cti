d73d28bee5caf29f13abd2a9c01f5ae1b4ee76ae
ssyybbe <134289646+ssyybbe@users.noreply.github.com>
2026-06-29 16:18:38 +0200

    Update creatio-hucc.js

--- creatio-hucc.js
+++ creatio-hucc.js
@@ -1,37 +1,16 @@
 window.addEventListener("VCUCCoreLoadDone", () => {
+    // Pour afficher "ça sonne" dans votre UI
+    Vocalcom.UCCore.addHandler("OnInboundCallRinging", function() {
+        var context = Vocalcom.UCCore.getGlobalContext();
+        console.log("Appel entrant de", context.callInfo.caller);
 
-    // 1. Appel entrant → chercher le contact dans Creatio
+    });
+
+
     Vocalcom.UCCore.addHandler("OnSearchForCaller", function(phoneNumber) {
-        console.log("aaaaaa");
+        console.log("OnSearchForCaller begin");
         console.log(phoneNumber);
         console.log(phoneNumber.E164);
-        console.log("aaaaaa");
-        //searchContactInCreatio(phoneNumber.E164).then(results => Vocalcom.UCCore.emitCallerSearchResult(results));
+        console.log("OnSearchForCaller end");
     });
-
-    // 2. Résultats prêts → ouvrir la fiche si un seul résultat
-/*    Vocalcom.UCCore.addHandler("OnCallerSearchResult", function(results) {
-        console.log("bbbb");
-        console.log(results);
-        if (results.length === 1) {
-            console.log(results[0]);
-            console.log("bbbbbb");
-            //navigateToCreatioRecord(results[0].objectId, results[0].objectType);
-        }
-    });
-
-    // 3. Appel terminé → logger dans Creatio
-    Vocalcom.UCCore.addHandler("OnCallFree", function() {
-        console.log("yoo");
-        //logCallInCreatio();
-    });
-
-    // 4. Agent qualifie l'appel → sauvegarder dans Creatio
-    Vocalcom.UCCore.addHandler("OnSetCallDisposition", function(callStatusData) {
-        console.log("cccccc");
-        console.log(callStatusData);
-        console.log("cccccc");
-        //saveDispositionInCreatio(callStatusData);
-    });
-*/
 });
